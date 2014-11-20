587f76e40b4f2047dd3d303ab64e1edb554d0c42
Peter Needle <pneedle@gmail.com>
2014-11-20 09:50:46 -0500

    PHP: permissiosn to view others w this aspect

--- sites/all/modules/custom/natal/aspect_code.php
+++ sites/all/modules/custom/natal/aspect_code.php
@@ -1,13 +1,10 @@
 <?php
+global $user;
 $title = drupal_get_title();  // e.g., "mercury_saturn_square"
 $components = explode('_',$title);
 $planet_0 = ucfirst($components[0]);
 $planet_1 = ucfirst($components[1]);
 $angle = $components[2];
-
-echo "planet1 = " . $planet_0 . "<br />";  // e.g., "Mercury"
-echo "planet2 = " . $planet_1 . "<br />";  // e.g., "Saturn"
-echo "aspect = " . $angle . "<br />";  // e.g., "square"
 
 $field = "natal_" . $title . "_value";  // e.g.,: "natal_mercury_saturn_square_value"
 $table = "field_data_natal_" . $title ;  // e.g., "field_data_natal_mercury_saturn_square"
@@ -15,6 +12,7 @@ $table = "field_data_natal_" . $title ;  // e.g., "field_data_natal_mercury_satu
 echo "<b>Other people with this aspect include:</b><br /><br />";
 
 $result = db_query("SELECT entity_id, $field FROM {$table}")->fetchAll(PDO::FETCH_ASSOC);
+
 foreach ($result as $record) {
 	if($record[$field] != NULL) {
 		$result2 = db_query("SELECT nid, title, type FROM {node}")->fetchAll(PDO::FETCH_ASSOC);
@@ -23,11 +21,35 @@ foreach ($result as $record) {
 				$full = explode('_',$record2['title']);
 				$first = ucfirst($full[1]);
 				$last = ucfirst($full[0]);
-				echo "<a href=\"../natal/" . $record2['title'] . "\"";
-				echo "<span class=\"fullname\" ; >"  . $first . " " . $last . ":</span></a>";
-				echo " " . $planet_0 . " " . $angle . " ". $planet_1 . " angle: " . $record[$field] . "&deg;<br /><br />";
+				$nid = $record2['nid'];
+
+				// code for permission to view "Other people with this aspect include:"
+				// Get a single value out of the database (Public or Private).
+				$pub_pri = db_query("SELECT natal_pub_pri_value FROM {field_data_natal_pub_pri} WHERE entity_id = $nid")->fetchField();
+
+				if ($user->uid == 0) { // When uid=0, then the user is "anonymous" ==> only view public records
+					if ($pub_pri == "Public") {
+//						echo $nid . "<br />";
+						echo "<a href=\"../natal/" . $record2['title'] . "\"";
+						echo "<span class=\"fullname\" ; >"  . $first . " " . $last . ":</span></a>";
+						echo " " . $planet_0 . " " . $angle . " ". $planet_1 . " angle: " . $record[$field] . "&deg;<br /><br />";
+					}
+				} else { // When (uid != 0), then the user has logged in ==> can view both private and public records
+					if (($pub_pri == "Private") || ($pub_pri == "Public")) {
+//						echo $nid . "<br />";
+						echo "<a href=\"../natal/" . $record2['title'] . "\"";
+						echo "<span class=\"fullname\" ; >"  . $first . " " . $last . ":</span></a>";
+						echo " " . $planet_0 . " " . $angle . " ". $planet_1 . " angle: " . $record[$field] . "&deg;<br /><br />";
+					}
+				}
+				
 			}
 		}
 	}
 }
+
+echo "planet1 = " . $planet_0 . "<br />";  // e.g., "Mercury"
+echo "planet2 = " . $planet_1 . "<br />";  // e.g., "Saturn"
+echo "aspect = " . $angle . "<br />";  // e.g., "square"
+
 ?>
